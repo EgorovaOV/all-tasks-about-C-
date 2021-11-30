145a923c11416ebd66f445552942da5a2fd13956
Olga <ove2501@mail.ru>
2021-11-30 22:02:33 +0500

    add task2_2

---
+++ task2_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Enter numA fnd numB");
+
+int numA = int.Parse(Console.ReadLine());
+
+int numB = int.Parse(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine("Larger number is");
+    Console.WriteLine(numA);
+    Console.WriteLine("Smaller naber is");
+    Console.WriteLine(numB);
+}
+else if (numA < numB)
+{
+    Console.WriteLine("Larger number is");
+    Console.WriteLine(numB);
+    Console.WriteLine("Smaller naber is");
+    Console.WriteLine(numA);
+}
+else
+{
+     Console.WriteLine("the numbers match");
+}
