0a692397ed99da5cbb5fd1d8dea0931806a8e712
Olga <ove2501@mail.ru>
2021-11-30 20:57:00 +0500

    add task 1 2

---
+++ task 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести квадрат числа
+Console.WriteLine("Enter number");
+int number = int.Parse(Console.ReadLine());
+
+int square = number*number;
+
+Console.WriteLine("square of number is");
+Console.WriteLine(square);
+
---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Enter numA and numB");
+
+int numA = int.Parse(Console.ReadLine());
+
+int numB = int.Parse(Console.ReadLine());
+
+if (numA / numB == numB)
+{
+    Console.WriteLine("numA is square of numB");
+}
+else 
+{
+    Console.WriteLine("numA is not square of numB");
+}
