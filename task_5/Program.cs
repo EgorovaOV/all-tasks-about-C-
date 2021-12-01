f68d16ace0b7a56ed7995442029394d25f70a863
Olga <ove2501@mail.ru>
2021-12-01 21:55:10 +0500

    add task_5 with ?

---
+++ task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+int a = int.Parse(Console.ReadLine());
+
+double y(double a)
+{
+    double result = 2 * a + a * a;
+    return result;
+}
+
+
+
+
