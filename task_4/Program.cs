4d5961059d4ac0ebd81522ab2968e1591f78adbc
Olga <ove2501@mail.ru>
2021-12-01 21:03:37 +0500

    add task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Enter num1");
+
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter num2");
+
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter num3");
+
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine("num1 is max");
+    }
+    else 
+    {
+       Console.WriteLine("num3 is max"); 
+    }
+}
+else if (num2 > num3)
+{
+    Console.WriteLine("num2 is max");
+}
