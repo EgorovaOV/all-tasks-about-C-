6aa86bb423c882d353f74acda713d5f41ec2aba0
Olga <ove2501@mail.ru>
2021-12-01 23:29:29 +0500

    add task3_1

---
+++ task3_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// По заданному номеру дня недели вывести его название
+
+string [] week = {"","mon", "thues", "wedn", "thurs", "fri", "sat", "sun"};
+
+Console.WriteLine("Enter day of week");
+
+int day = int.Parse(Console.ReadLine());
+
+Console.WriteLine(week [day]);
