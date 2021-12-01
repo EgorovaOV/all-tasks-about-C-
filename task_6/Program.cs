dcf9b038aeda7fe7808ee054fb90389491cc2977
Olga <ove2501@mail.ru>
2021-12-01 23:12:27 +0500

    add task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Enter number");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("number is even ");
+}
+else
+{
+Console.WriteLine("number is not even ");
+}
+
