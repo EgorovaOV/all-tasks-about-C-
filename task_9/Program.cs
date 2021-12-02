66e7802e323066cb723913c314776144ad42c3ca
Olga <ove2501@mail.ru>
2021-12-02 22:59:28 +0500

    add task_9

---
+++ task_9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("enter number");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num <= 999 & num > 99)
+{
+    Console.WriteLine("Third digit of nunber is");
+    Console.WriteLine(num % 10);
+}
+else
+{
+    Console.WriteLine("Number is not three digit");
+}
