d37cbc0939f7f1a1e34aa42ac7d1e89964c81f62
Olga <ove2501@mail.ru>
2021-12-02 21:38:27 +0500

    add task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Enter number");
+
+int n = int.Parse(Console.ReadLine());
+
+int start = 1;
+
+int even_n = (start+1);
+
+while (even_n <= n)
+{
+    if (even_n % 2 == 0)
+    {
+        Console.WriteLine(even_n);
+    }
+    even_n = (even_n+2);
+}
