2c11cce6048a132ca321193736256475a96e753a
Olga <ove2501@mail.ru>
2021-12-04 09:54:18 +0500

    add task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random (). Next (10, 99);
+
+Console.WriteLine($"Your rabdom number is {number}");
+
+int first = (number / 10);
+
+int second = (number % 10);
+
+if (first > second)
+{
+    Console.WriteLine($"Largest digit of number is {first}");
+}
+else if (first < second)
+{
+    Console.WriteLine($"Largest digit of number is {second}");
+}
+else 
+{
+    Console.WriteLine("Digits of number is same");
+}
