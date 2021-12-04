8a8dbdcc98166e783d230a6c09d71455ad30f5e3
Olga <ove2501@mail.ru>
2021-12-04 10:38:27 +0500

    add task_12

---
+++ task_12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Enter number_A and number_B");
+
+int number_A = int.Parse(Console.ReadLine());
+
+int number_B = int.Parse(Console.ReadLine());
+
+if (number_A % number_B == 0)
+{
+    Console.WriteLine("number_A is multiple of number_B");
+}
+
+else if (number_B > number_A)
+{
+    Console.WriteLine("0");
+}
+else
+{
+    Console.WriteLine(number_A % number_B);
+}
