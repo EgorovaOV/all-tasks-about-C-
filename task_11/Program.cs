ea0983b148761dff19ace5cfc3821c669d32635a
Olga <ove2501@mail.ru>
2021-12-04 10:11:57 +0500

    add task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int number = new Random(). Next (100 , 999);
+
+Console.WriteLine($"Your random namber is {number}");
+
+int first = number / 100;
+
+int third = number % 10;
+
+int new_number = (first * 10 + third);
+
+Console.WriteLine($"Your new namber is {new_number}");
