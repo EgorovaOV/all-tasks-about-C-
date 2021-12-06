f8d34b94a57bcf784b84bc3eb48b9edbdc62f62f
Olga <ove2501@mail.ru>
2021-12-06 21:34:44 +0500

    add task_17 again

---
+++ task_17/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Enter day of week");
+
+int day = int.Parse(Console.ReadLine());
+
+if (day > 7 | day <= 0)
+{
+Console.WriteLine("you enter wrong day");
+}
+else if (day == 7)
+{
+  Console.WriteLine("it is sunday");
+}
+else if (day == 6)
+{
+  Console.WriteLine("it is saturday");
+}
+else Console.WriteLine("sorry");
