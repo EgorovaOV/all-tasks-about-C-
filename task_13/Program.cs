01137fad6667c3dd7e6bc984af93a7d07d10d3d7
Olga <ove2501@mail.ru>
2021-12-07 23:28:26 +0500

    add task_13

---
+++ task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int number = int.Parse(Console.ReadLine());
+
+int result = 0;
+
+if (number > 999)
+{
+    result = (number / 10);
+    while (result > 999)
+    {
+        result = (result / 10);
+    }
+    Console.WriteLine($"third digit of number is {result % 10}");
+}
+else if (number < 100)
+{
+    Console.WriteLine("there is no third digit of number");
+}
+else Console.WriteLine(number % 10);
+
+
