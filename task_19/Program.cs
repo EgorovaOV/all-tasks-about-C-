c000cf7e125d105d82a36521cf98f3d87b542f2d
Olga <ove2501@mail.ru>
2021-12-09 22:38:37 +0500

    add task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool[] array = { false, true };
+
+bool x = array[0];
+
+bool y = array[0];
+
+bool result = false;
+
+for (int i = 0; i <= array.Length; i++)
+{
+    for (int j = 0; j <= array.Length; j++)
+    {
+        if ((!(array[i] | array[j])) == ((!array[i]) & (!array[j])))
+        {
+            Console.WriteLine("statment is true");
+        }
+        else
+        {
+            Console.WriteLine("statment is false");
+            break;
+        }
+    }
+}
+
+
+
