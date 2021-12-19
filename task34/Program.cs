058fb5e7cf983c35de56a01a105704021eb5fb14
Olga <ove2501@mail.ru>
2021-12-19 21:12:54 +0500

    add task34

---
+++ task34/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу замену элементов массива на противоположные
+double AntiFill (double [] ar)
+{
+    int i = 0;
+    int index = ar.Length;
+    while (i < index)
+    {
+        ar[i] = -ar[i];
+        i++;
+    }
+    return ar [i];
+    Console.WriteLine(ar[i]);
+}
+double array = { 1 , -2, 6 , -4, 8.2, 1};
+
+AntiFill (array);
+
