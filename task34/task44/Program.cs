c6dbef8f38f75f482ea11458776a777615889b13
Olga <ove2501@mail.ru>
2021-12-27 21:00:40 +0500

    add task_44

---
+++ task34/task44/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+int b = 2, bb = 3, k = 4, kk = 6;
+
+void InterPoint (double k1, double b1, double k2, double b2)
+{
+double a = double.Parse(Console.ReadLine());
+double aa = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double bb = double.Parse(Console.ReadLine());
+
+double x = (aa / a);
+double y = (bb /b);
+Console.WriteLine(x);
+Console.WriteLine(y);
+if (x == y)
+{
+    Console.WriteLine("Straights are same ");
+}
+
+else Console.WriteLine($"Straights intersect when X equal {((bb-b)/(a-aa))}");
+ 
+}
+
+
+InterPoint (b, bb,k, kk);
+
+
