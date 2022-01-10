edb4ce76696cf0929c849d3524f2567ec3284bf4
Olga <ove2501@mail.ru>
2022-01-10 23:44:47 +0500

    add task70

---
+++ task_70/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти сумму цифр числа
+//если чиcло меньше 99, делим его на 10 с остатком
+Console.WriteLine("Hello, World!");
+
+int Sum (int a);
+{
+    int a = int.Parse(Console.ReadLine());
+    int sum = (a % 10 + a /10);
+
+    if (a < 100) return sum ;
+else return Sum;
+Console.WriteLine(sum);
+}
+
+//int a = int.Parse(Console.ReadLine());
+
+Sum (a);
+//int sum = 0;
+
+
+
